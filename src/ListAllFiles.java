8a40502bf9845e7b37c66e72b568001ff71991ed
nico <nico@imac-pro.fritz.box>
2020-07-16 18:17:09 +0200

    first commit

---
+++ src/ListAllFiles.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import acm.program.ConsoleProgram;
+
+public class ListAllFiles extends ConsoleProgram {
+	private File dir;
+	private List<String> content = new ArrayList<String>();
+
+	public void run() {
+		dir = new File(readLine("Enter directory to scan (e.g. files): "));
+		File[] files = dir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			println(i + ": " + files[i].toString());
+			readFileContent(files[i]);
+		}
+		println("\n...................................................\n");
+		for (int i = 0; i < content.size(); i++) {
+			println(content.get(i));
+		}
+	}
+
+	private void readFileContent(File file) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while (true) {
+				String line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				content.add(line);
+			}
+
+		} catch (IOException e) {
+			println("File not found!");
+			e.printStackTrace();
+		}
+	}
+}
